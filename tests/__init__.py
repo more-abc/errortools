c7d66c53920d6eff2d3805f44d5fcd8dde156a9b
Evan Yang <quantbit@126.com>
2026-04-06 13:16:00 +0800

    Delete unuse imports in tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,3 @@
 """Tests for `errortools` module. Using pytest."""
 
-__version__ = "0.29"
+__version__ = "1.0"
--- tests/test_logging.py
+++ tests/test_logging.py
@@ -3,7 +3,6 @@
 from __future__ import annotations
 
 import io
-import tempfile
 import threading
 from pathlib import Path
 
