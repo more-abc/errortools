e5f9e32647a95e6d0a430829971f146940e4072b
Evan Yang <quantbit@126.com>
2026-04-19 12:49:36 +0800

    Use Final in const.py

--- _errortools/const.py
+++ _errortools/const.py
@@ -1,10 +1,11 @@
 """Constants for errortools module."""
 
+from typing import Final
 # ------------------------------------------------------------------
 # error_cache constants
 # ------------------------------------------------------------------
 
-DEFAULT_ERROR_CACHE_SIZE: int = 128
-SMALL_ERROR_CACHE_SIZE: int = 64
-LARGE_ERROR_CACHE_SIZE: int = 1024
-UNLIMITED_ERROR_CACHE: None = None
+DEFAULT_ERROR_CACHE_SIZE: Final[int] = 128
+SMALL_ERROR_CACHE_SIZE: Final[int] = 64
+LARGE_ERROR_CACHE_SIZE: Final[int] = 1024
+UNLIMITED_ERROR_CACHE: Final[None] = None
--- _errortools/version.py
+++ _errortools/version.py
@@ -1,5 +1,5 @@
-__version__: str = "2.3.0"
-__version_tuple__: tuple[int, int, int] = (2, 3, 0)
+__version__: str = "2.3.1"
+__version_tuple__: tuple[int, int, int] = (2, 3, 1)
 __commit_id__: str | None = None
 
 version = __version__
