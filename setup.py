203b2c478f4a666d2454555f0da02248955338a5
Evan Yang <quantbit@126.com>
2026-05-01 15:08:12 +0800

    Version: 2.5.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import sys
 
 from setuptools import setup, find_packages
 
-_VERSION: str = "2.4.4"
+_VERSION: str = "2.5.0"
 _CLI_INFO: dict[str, list[str]] = {
     "console_scripts": [
         "python -m errortools = _errortools.cli:main",
