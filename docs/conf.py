adf8fbd72c1cd0afee829005c69afadde4194a94
Evan Yang <quantbit@126.com>
2026-05-31 20:29:26 +0800

    Fix bugs again in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,11 +6,12 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-from _errortools.version import __version__, __version_tuple__
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + "/..")
+
+from _errortools.version import __version__, __version_tuple__
 
 
 project = "errortools"
