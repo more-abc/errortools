a63aac5570ab9c97d07e60e11d5934ba47abc887
Evan Yang <quantbit@126.com>
2026-05-31 19:08:12 +0800

    Fix bugs in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,11 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
+
 
 from _errortools.version import __version__, __version_tuple__
 
