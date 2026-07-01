aa2300082977649a15c59559ede8e14bfe9b9002
Evan Yang <quantbit@126.com>
2026-07-01 17:36:18 +0800

    Dump version in errortools_speedbelt

--- errortools_speedbelt/__init__.py
+++ errortools_speedbelt/__init__.py
@@ -9,6 +9,6 @@ from errortools import VersionInfo
 
 from .support_c_ext import ERRORTOOLS_SUPPORTS_C_EXTENSIONS  # noqa: F401
 
-__version__: Final[str] = "3.6.0"
+__version__: Final[str] = "0.1.1"
 __version_info__: Final[VersionInfo] = VersionInfo.from_str(__version__)
 __version_tuple__: Final[tuple[int, int, int]] = __version_info__.to_tuple()
